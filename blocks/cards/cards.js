6837fab614e0f40289de618375ddc30b3b6c282d
Jakesh J <jakesh.j@infosys.com>
2026-09-01 00:44:19 +0530

    fixed cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,6 +6,15 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
+    if (li.children.length === 1) {
+      const body = li.firstElementChild;
+      const picture = body.querySelector('picture');
+      if (picture && body.children.length > 1) {
+        const image = document.createElement('div');
+        image.append(picture);
+        li.prepend(image);
+      }
+    }
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
